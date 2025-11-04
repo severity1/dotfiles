e8ab896fa2ed34953d77ed91f1863f713b0f652b
severity1 <johnreilly.pospos@gmail.com>
2025-11-04 17:29:22 +1300

    Update Neovim configuration with plugin enhancements
    - Add catppuccin colorscheme option to lazy.lua
    - Simplify toggleterm configuration by removing complex size function
    - Add snacks.lua plugin configuration

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -3,19 +3,13 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     keys = {
-      { "<C-\\>", "<cmd>ToggleTerm direction=horizontal size=20<cr>", desc = "Toggle Terminal" },
+      { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
       { "<C-\\>", "<C-\\><C-n><cmd>ToggleTerm<cr>", mode = "t", desc = "Toggle Terminal" },
     },
     opts = {
-      size = function(term)
-        if term.direction == "horizontal" then
-          return 20
-        elseif term.direction == "vertical" then
-          return vim.o.columns * 0.4
-        end
-      end,
-      open_mapping = [[<C-\>]],
+      size = 20,
       direction = "horizontal",
+      open_mapping = [[<C-\>]],
       shade_terminals = true,
       persist_size = true,
       persist_mode = true,
